3db0c80cabec2394193f6379ad754b619cdbc64c
alexisbarros <apenasalexis@gmail.com>
2025-12-17 23:40:06 -0300

    :recycle: refactor condition evaluation logic in KuAutocomplete

--- src/modules/invitation/components/invitation-form.tsx
+++ src/modules/invitation/components/invitation-form.tsx
@@ -58,7 +58,7 @@ export default function InvitationForm({
           isUnique={true}
           isDisabled={loading}
           isAutofocus={false}
-          conditions={[]}
+          conditions={{}}
           formState={{ roleId: invitation.roleId || "" }}
         />
       </div>

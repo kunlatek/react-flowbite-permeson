714ec3aafb010298d38ddf25e7a972a8d36fcebc
alexisbarros <apenasalexis@gmail.com>
2025-12-28 18:23:51 -0300

    :art: improve styling and layout in KuWysiwyg component for better content display

--- src/components/ku-components/flowbite/form/ku-wysiwyg.tsx
+++ src/components/ku-components/flowbite/form/ku-wysiwyg.tsx
@@ -51,7 +51,7 @@ export const KuWysiwyg = (props: IKuWysiwygProps) => {
       </div>
 
       <div
-        className={`rounded-lg border ${
+        className={`rounded-lg border overflow-hidden ${
           hasError
             ? 'border-red-500 focus-within:border-red-500 focus-within:ring-red-500'
             : 'border-gray-300 dark:border-gray-600 focus-within:border-cyan-500 focus-within:ring-cyan-500'
@@ -68,7 +68,7 @@ export const KuWysiwyg = (props: IKuWysiwygProps) => {
           placeholder={placeholder || 'Digite seu conteúdo...'}
           modules={quillModules}
           theme="snow"
-          className="h-full [&_.ql-container]:min-h-[200px] [&_.ql-editor]:text-gray-900 dark:[&_.ql-editor]:text-white [&_.ql-editor]:min-h-[200px] [&_.ql-toolbar]:border-0 [&_.ql-container]:border-0 [&_.ql-toolbar]:border-b [&_.ql-toolbar]:border-gray-200 dark:[&_.ql-toolbar]:border-gray-600 [&_.ql-toolbar]:rounded-t-lg [&_.ql-container]:rounded-b-lg"
+          className="h-full max-w-full [&_.ql-container]:min-h-[200px] [&_.ql-container]:overflow-hidden [&_.ql-editor]:text-gray-900 dark:[&_.ql-editor]:text-white [&_.ql-editor]:min-h-[200px] [&_.ql-editor]:break-words [&_.ql-editor]:break-all [&_.ql-toolbar]:border-0 [&_.ql-container]:border-0 [&_.ql-toolbar]:border-b [&_.ql-toolbar]:border-gray-200 dark:[&_.ql-toolbar]:border-gray-600 [&_.ql-toolbar]:rounded-t-lg [&_.ql-container]:rounded-b-lg"
           style={{ minHeight: `${height}px` }}
         />
       </div>

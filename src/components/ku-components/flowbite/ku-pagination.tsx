7e4908dc883561a4b39d2ad1431bd7efcde3a5df
alexisbarros <apenasalexis@gmail.com>
2026-01-01 20:19:19 -0300

    :sparkles: add pagination localization strings in English, Spanish, and Portuguese, and integrate translation support in KuPagination component

--- src/components/ku-components/flowbite/ku-pagination.tsx
+++ src/components/ku-components/flowbite/ku-pagination.tsx
@@ -1,8 +1,11 @@
 import { Pagination } from "flowbite-react";
+import { useTranslation } from "react-i18next";
 import { IKuPaginationProps } from "@/interfaces/ku-components";
 
 export const KuPagination = (props: IKuPaginationProps) => {
   const { id, testId, currentPage, totalPages, onPageChange } = props;
+  const { t } = useTranslation();
+  
   return (
     <div className="flex justify-center" id={id} data-testid={testId}>
       <Pagination
@@ -10,6 +13,8 @@ export const KuPagination = (props: IKuPaginationProps) => {
         totalPages={totalPages}
         onPageChange={onPageChange}
         showIcons
+        previousLabel={t("common.pagination.previous")}
+        nextLabel={t("common.pagination.next")}
       />
     </div>
   );

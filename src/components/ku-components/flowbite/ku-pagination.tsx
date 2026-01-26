1800d7136b0013ab51cae24beb86ec2a2fcd4d2c
alexisbarros <apenasalexis@gmail.com>
2026-01-25 22:42:08 -0300

    :sparkles: enhance KuDataTable and KuPagination components with mobile responsiveness, adding a new useIsMobileOrTablet hook for adaptive rendering and improving user experience across devices

--- src/components/ku-components/flowbite/ku-pagination.tsx
+++ src/components/ku-components/flowbite/ku-pagination.tsx
@@ -1,10 +1,12 @@
 import { Pagination } from "flowbite-react";
 import { useTranslation } from "react-i18next";
 import { IKuPaginationProps } from "@/interfaces/ku-components";
+import { useIsMobileOrTablet } from "@/hooks/use-is-mobile";
 
 export const KuPagination = (props: IKuPaginationProps) => {
   const { id, testId, currentPage, totalPages, onPageChange } = props;
   const { t } = useTranslation();
+  const isMobile = useIsMobileOrTablet();
   
   return (
     <div className="flex justify-center" id={id} data-testid={testId}>
@@ -15,6 +17,7 @@ export const KuPagination = (props: IKuPaginationProps) => {
         showIcons
         previousLabel={t("common.pagination.previous")}
         nextLabel={t("common.pagination.next")}
+        layout={isMobile ? "navigation" : undefined}
       />
     </div>
   );
